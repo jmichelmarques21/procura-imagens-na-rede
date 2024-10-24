9078306400bf6dace880bfc6fbdddf6f32f1939c
Jean Michel Marques <jean.marques@KAPAZI.local>
2024-10-24 09:40:26 -0300

    salva uma copia da imagem em uma pasta nomeada com o sku ou termo

--- procuraImagem.py
+++ procuraImagem.py
@@ -1,12 +1,15 @@
 import os
 import fnmatch
+import shutil
 
-def procurar_imagens(diretorio_principal, sku_nome_produto):
+def procurar_e_copiar_imagens(diretorio_principal, sku_nome_produto, diretorio_destino):
     # Extensões de arquivos de imagem que queremos procurar
     formatos = ['*.jpg', '*.jpeg', '*.png']
     
-    # Lista para armazenar os caminhos dos arquivos encontrados
-    imagens_encontradas = []
+    # Criar a pasta de destino com o nome do SKU (caso não exista)
+    destino_sku = os.path.join(diretorio_destino, sku_nome_produto)
+    if not os.path.exists(destino_sku):
+        os.makedirs(destino_sku)
 
     # Percorre todas as pastas e arquivos no diretório principal
     for raiz, diretorios, arquivos in os.walk(diretorio_principal):
@@ -15,15 +18,14 @@ def procurar_imagens(diretorio_principal, sku_nome_produto):
             for arquivo in fnmatch.filter(arquivos, formato):
                 if sku_nome_produto.lower() in arquivo.lower():
                     caminho_completo = os.path.join(raiz, arquivo)
-                    imagens_encontradas.append(caminho_completo)
-
-    return imagens_encontradas
+                    # Copia a imagem para o diretório de destino
+                    destino_arquivo = os.path.join(destino_sku, arquivo)
+                    shutil.copy2(caminho_completo, destino_arquivo)
+                    print(f'Imagem copiada: {destino_arquivo}')
 
 # Exemplo de uso
-diretorio = r'\\desk181-mkt\Fotos Geral'
+diretorio_origem = r'\\desk181-mkt\Fotos Geral'  # Diretório na rede onde estão as imagens
 sku_ou_nome = '01440101'  # Pode ser o código SKU ou o nome do produto
-imagens = procurar_imagens(diretorio, sku_ou_nome)
+diretorio_destino = r'C:\Users\jean.marques\Desktop\procura-imagens-python'  # Diretório onde as imagens serão copiadas
 
-# Exibindo os arquivos encontrados
-for imagem in imagens:
-    print(imagem)
+procurar_e_copiar_imagens(diretorio_origem, sku_ou_nome, diretorio_destino)
